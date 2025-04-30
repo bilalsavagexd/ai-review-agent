972bde61bdb3632fbde53ac685efac3eb67b62ba
Syed Ahmed Haseeb <167350234+haseeb557@users.noreply.github.com>
2025-04-30 23:14:22 +0500

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,1 +1,27 @@
-console.log("Hello, world! My name is Syed Ahmed Haseeb");
+a=1; b=2; c=3
+function DoSomething(x){
+if(x==null){x=5}
+if(x>0){
+if(x<10){
+for(i=0;i<10;i++){
+if(i%2==0){
+console.log("Even:"+i)
+}else{
+console.log("Odd:"+i)
+}
+}
+}else{
+while(x>0){
+console.log("Counting down:"+x)
+x--
+}
+}
+}else{
+console.log("Zero or negative")
+}
+var result=x+b+a+c
+return result
+}
+DoSomething()
+DoSomething("hello")
+DoSomething(20)
